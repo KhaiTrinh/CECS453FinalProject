922d2293a017021497fc2b1efef06f08f794e2b3
Khai Trinh <khaitrinh99@gmail.com>
2021-06-25 00:31:37 -0700

    Moved code for register layout to the register class.

--- app/src/main/java/com/procode/imagevault/RegisterActivity.java
+++ app/src/main/java/com/procode/imagevault/RegisterActivity.java
@@ -1,8 +1,10 @@
 package com.procode.imagevault;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +12,23 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        // calling the action bar
+        Toolbar toolbar = findViewById(R.id.registerToolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+    }
+
+    // this event will enable the back
+    // function to the button on press
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
