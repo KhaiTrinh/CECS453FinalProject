ced531cfd4e64b1fc5997dce3981c7dc25da98e7
Nikko Chan <56482580+gnikkoch96@users.noreply.github.com>
2021-06-24 17:24:21 -0700

    added the back button in the support action bar

--- app/src/main/java/com/procode/imagevault/MainActivity.java
+++ app/src/main/java/com/procode/imagevault/MainActivity.java
@@ -1,8 +1,12 @@
 package com.procode.imagevault;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        // calling the action bar
+        Toolbar toolbar = findViewById(R.id.registerToolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+    }
+
+    // this event will enable the back
+    // function to the button on press
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
