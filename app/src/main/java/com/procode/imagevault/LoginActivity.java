74059fd4b634519384600ade638c75e5f5175f30
Khai Trinh <khaitrinh99@gmail.com>
2021-06-27 19:31:47 -0700

    Rough implementation of Login authentication.

--- app/src/main/java/com/procode/imagevault/LoginActivity.java
+++ app/src/main/java/com/procode/imagevault/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.procode.imagevault;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -13,6 +14,9 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -43,7 +47,7 @@ public class LoginActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
-        mEmail = findViewById(R.id.etRegisterEmail);
+        mEmail = findViewById(R.id.etLoginEmail);
         mPassword = findViewById(R.id.etLoginPassword);
         mLogin = findViewById(R.id.btnLogin);
         mRememberMe = findViewById(R.id.cbRememberMe);
@@ -58,7 +62,9 @@ public class LoginActivity extends AppCompatActivity {
 //        super.onStart();
 //
 //        FirebaseUser currentUser = mAuth.getCurrentUser();
-//        if (currentUser != null) { // User is already signed in
+//        if (currentUser != null && settings.getBoolean("rememberUser", false)) { // User is already signed in
+//            FirebaseAuth.getInstance().signOut();
+//        } else if (currentUser != null && settings.getBoolean("rememberUser", true)) {
 //            Intent intent = new Intent(getApplicationContext(), VaultActivity.class);
 //            startActivity(intent);
 //        }
@@ -68,16 +74,24 @@ public class LoginActivity extends AppCompatActivity {
         mLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                boolean correctCredentials = false;
-
-                if (correctCredentials) {
-                    Intent intent = new Intent(getApplicationContext(), VaultActivity.class);
-                    startActivity(intent);
-                } else {
-                    // Clear password field if entered information is incorrect
-                    mPassword.getText().clear();
-                    Toast.makeText(LoginActivity.this, "Incorrect username or password", Toast.LENGTH_SHORT).show();
-                }
+                String email = mEmail.getText().toString();
+                String password = mPassword.getText().toString();
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                Intent intent = new Intent(getApplicationContext(), VaultActivity.class);
+                                startActivity(intent);
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Clear password field if entered information is incorrect
+                                mPassword.getText().clear();
+                                Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
             }
         });
 
