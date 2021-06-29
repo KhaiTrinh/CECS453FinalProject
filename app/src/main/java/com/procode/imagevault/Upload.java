8926b19b7b549cb5a117d44b392dd7f59994671a
Khai Trinh <khaitrinh99@gmail.com>
2021-06-29 01:31:31 -0700

    First successful upload of image onto the cloud.

--- app/src/main/java/com/procode/imagevault/Upload.java
+++ app/src/main/java/com/procode/imagevault/Upload.java
@@ -1,5 +1,22 @@
 package com.procode.imagevault;
 
 public class Upload {
+    private String mName, mImageUrl;
 
+    public Upload() {}
+
+    public Upload(String name, String imageUrl) {
+        if (name.trim().equals("")) name = "Untitled";
+
+        mName = name;
+        mImageUrl = imageUrl;
+    }
+
+    public String getName() { return mName; }
+
+    public void setName(String name) { mName = name; }
+
+    public String getImageUrl() { return mImageUrl; }
+
+    public void setImageUrl(String imageUrl) { mImageUrl = imageUrl; }
 }
--- app/src/main/java/com/procode/imagevault/activities/UploadActivity.java
+++ app/src/main/java/com/procode/imagevault/activities/UploadActivity.java
@@ -1,19 +1,34 @@
 package com.procode.imagevault.activities;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.procode.imagevault.R;
+import com.procode.imagevault.Upload;
 
 public class UploadActivity extends AppCompatActivity {
 
@@ -27,6 +42,10 @@ public class UploadActivity extends AppCompatActivity {
 
     private Uri mImageUri;
 
+    private StorageReference mStorageRef;
+    private DatabaseReference mDatabaseRef;
+    private String mDirectoryName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +58,11 @@ public class UploadActivity extends AppCompatActivity {
         mPreview = findViewById(R.id.ivPreview);
         mProgressBar = findViewById(R.id.uploadProgressBar);
         setClickListeners();
+
+
+        mDirectoryName = "images/" + FirebaseAuth.getInstance().getCurrentUser().getUid();
+        mStorageRef = FirebaseStorage.getInstance().getReference(mDirectoryName);
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference(mDirectoryName);
     }
 
     private void setClickListeners() {
@@ -52,7 +76,7 @@ public class UploadActivity extends AppCompatActivity {
         mUpload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                uploadFile();
             }
         });
 
@@ -82,6 +106,58 @@ public class UploadActivity extends AppCompatActivity {
             mImageUri = data.getData();
 
             mPreview.setImageURI(mImageUri);
+        }
+    }
+
+    // Returns the file extension of the image
+    private String getFileExtension(Uri uri) {
+        ContentResolver cr = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(cr.getType(uri));
+    }
+
+    private void uploadFile() {
+        if (mImageUri != null) {
+            StorageReference fileReference = mStorageRef.child(System.currentTimeMillis()
+            + "." + getFileExtension(mImageUri));
+
+            fileReference.putFile(mImageUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Handler handler = new Handler();
+                            handler.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    mProgressBar.setProgress(0);
+                                }
+                            }, 3000);
+
+                            Toast.makeText(UploadActivity.this, "Upload successful", Toast.LENGTH_SHORT).show();
+                            Upload upload = new Upload(mFilename.getText().toString().trim(),
+                                    taskSnapshot.getMetadata().getReference().getDownloadUrl().toString());
+
+                            // Creates a unique id for each item uploaded
+                            String uploadId = mDatabaseRef.push().getKey();
+                            mDatabaseRef.child(uploadId).setValue(upload);
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(UploadActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                            double progress = (100.0 * snapshot.getBytesTransferred() / snapshot.getTotalByteCount());
+                            mProgressBar.setProgress((int) progress);
+                        }
+                    });
+
+        } else {
+            Toast.makeText(this, "No file selected", Toast.LENGTH_SHORT).show();
         }
     }
 }
